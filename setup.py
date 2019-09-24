0e51ac161aca607593ffa10a6ea336eeed3f8d83
Gregory Lindsey <gclindsey@gmail.com>
2019-09-24 15:13:21 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting the age of individuals based on physical characteristics.',
+    author='Gregory Lindsey',
+    license='MIT',
+)
